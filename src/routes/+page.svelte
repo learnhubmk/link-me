4b4592ced41e9c0eac623b05bac5cc10cb9432e5
Martin Zhivkovikj <martin.zivkovik@gmail.com>
2024-04-13 17:28:29 +0200

    add Tailwind setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Добредојде на Вмрежи се</h1>
-<p>Посети ја <a href="/about">„За нас“</a> страницата</p>
+<h1 class="text-3xl font-bold underline">
+    Добредојде на Вмрежи се
+  </h1>
+  
+  <p class="text-3xl">Посети ја <a class="text-3xl font-bold underline" href="/about" >„За нас“</a> страницата</p>
+
+
+  
