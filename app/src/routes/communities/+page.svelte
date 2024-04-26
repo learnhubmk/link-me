4ca5a141aa177cc3f13cc757888b5b74abbe79d8
Branko Bjadov <brankobjadov@gmail.com>
2024-04-26 21:35:38 +0200

    #13 use await directive to render api result

--- app/src/routes/communities/+page.svelte
+++ app/src/routes/communities/+page.svelte
@@ -2,6 +2,8 @@
     import {onMount} from 'svelte';
 
     let communities;
+    const loadCommunities = () => fetch('http://localhost:1337/api/communities').then(res => res.json());
+
     onMount(async () => {
         const response = await fetch('http://localhost:1337/api/communities');
         const data = await response.json();
@@ -13,13 +15,17 @@
 
 <div class="container p-6 mx-auto text-center bg-yellow-100 rounded mt-12 text-black">
     Example rendering communities from api:
-    {#if communities}
+
+    {#await loadCommunities()}
+        <!-- promise is pending -->
+        <p>waiting for the promise to resolve...</p>
+    {:then data}
         <ul>
-        {#each communities as community}
-            <li>{community.attributes.name} - {community.attributes.description}</li>
-        {/each}
+            {#each data.data as community}
+                <li>{community.attributes.name} - {community.attributes.description}</li>
+            {/each}
         </ul>
-    {:else}
-        <p>no communities</p>
-    {/if}
+    {:catch error}
+        <p>Something went wrong: {error.message}</p>
+    {/await}
 </div>
