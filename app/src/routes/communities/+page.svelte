26417d72c5d2a92ecab86e7ba190f28228138b47
Branko Bjadov <brankobjadov@gmail.com>
2024-04-26 21:36:32 +0200

    #13 code cleanup

--- app/src/routes/communities/+page.svelte
+++ app/src/routes/communities/+page.svelte
@@ -1,24 +1,12 @@
 <script>
-    import {onMount} from 'svelte';
-
-    let communities;
     const loadCommunities = () => fetch('http://localhost:1337/api/communities').then(res => res.json());
-
-    onMount(async () => {
-        const response = await fetch('http://localhost:1337/api/communities');
-        const data = await response.json();
-
-        communities = data.data;
-        console.log(communities);
-    });
 </script>
 
 <div class="container p-6 mx-auto text-center bg-yellow-100 rounded mt-12 text-black">
     Example rendering communities from api:
 
     {#await loadCommunities()}
-        <!-- promise is pending -->
-        <p>waiting for the promise to resolve...</p>
+        <p>Loading communities...</p>
     {:then data}
         <ul>
             {#each data.data as community}
