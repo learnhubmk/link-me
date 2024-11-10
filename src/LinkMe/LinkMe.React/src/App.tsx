f2d89cd5dd37cc312011ddd4919a675dc9ca198a
Dragan Dimkovikj <dragan.dimkovik@gmail.com>
2024-11-10 22:19:13 +0100

    Setup React with Tailwind CSS
    Created new React app with Tailwind CSS.
    Created a feature-based React folder structure
    Added dummy component

---
+++ src/LinkMe/LinkMe.React/src/App.tsx
@@ -0,0 +1,13 @@
+import WelcomeText from "./components/WelcomeText.tsx"
+
+function App() {
+  return (
+    <>
+      <WelcomeText />
+      <WelcomeText />
+      <WelcomeText />
+    </>
+  )
+}
+
+export default App
---
+++ src/LinkMe/LinkMe.React/src/components/WelcomeText.tsx
@@ -0,0 +1,10 @@
+const WelcomeText = () => {
+	return <>
+	<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+	</>
+
+}
+
+export default WelcomeText
