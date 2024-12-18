bfb75517008de2e1c25bad2407feaf6d83845c5e
Ljupcho1982 <99423204+Ljupcho1982@users.noreply.github.com>
2024-12-18 12:43:45 +0100

    Implement GetCommunity endpoint (#34)
    Implemented GetCommunity action in controller
    Implemented GetCommunity Handler
    Refactored IBaseRepository  Get to GetAsync

---
+++ src/LinkMe/LinkMe.ApplicationServices/Communities/GetCommunity.cs
@@ -0,0 +1,28 @@
+﻿using LinkMe.Domain;
+using LinkMe.Domain.Contracts;
+
+using MediatR;
+
+namespace LinkMe.ApplicationServices.Communities
+{
+    public class GetCommunity
+    {
+        public record Request(int Id)
+        : IRequest<Community>
+        {
+           
+        }
+
+        public class Handler(IBaseRepository<Community> _repository)
+            : IRequestHandler<Request, Community>
+        {
+            public async Task<Community> Handle(Request request, CancellationToken cancellationToken)
+            {
+              
+                var result = await _repository.GetAsync(request.Id);
+
+                return  result;
+            }
+        }
+    }
+}
